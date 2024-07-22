952ab684d67049c4cb119ef684dd487fe00f1cd0
upmanpransh <upmanprashun987@gmail.com>
2024-07-22 11:24:25 +0530

    Solve the error

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -44,7 +44,7 @@ export const Publish=()=>{
 function TextEditor({onChange}:{onChange:(e:ChangeEvent<HTMLTextAreaElement>)=>void}){
     return <div>
            <div className=" py-2 bg-white rounded-b-lg">
-               <textarea onChange={onChange} id="editor" rows="8" className="block w-full px-2 py-1 text-sm text-gray-800 bg-white border bg-slate-50 border-slate-300 rounded-lg focus:ring-0 " placeholder="Write an article..." required ></textarea>
+               <textarea onChange={onChange} id="editor" rows={8} className="block w-full px-2 py-1 text-sm text-gray-800 bg-white border bg-slate-50 border-slate-300 rounded-lg focus:ring-0 " placeholder="Write an article..." required ></textarea>
            </div>
        
     </div>
